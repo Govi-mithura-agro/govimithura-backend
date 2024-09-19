14bca994f2be1cfa514c75923f01b1081a0b7f92
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-09-19 15:45:08 +0530

    app-user-login

--- routes/AppUserRoute.js
+++ routes/AppUserRoute.js
@@ -31,6 +31,38 @@ router.route('/register').post(async (req, res) => {
     }
 });
 
+router.route('/login').post(async (req, res) => {
+
+    const {
+        email,
+        password
+    } = req.body;
+
+    try {
+        const user = await AppUsers.findOne({ email: email, password: password });
+
+        if (user) {
+
+            const loginUser = {
+
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                contact: user.contact
+                
+            }
+
+            return res.status(200).json({ status: "Login Success", loginUser});        
+        } 
+        else {
+            return res.status(500).json({ status: "The email or password is incorrect" });
+        }
+
+    } catch (error) {
+        return res.status(500).json({ status: "Error during login", message: error });
+    }
+});
+
 
 
 module.exports = router;
